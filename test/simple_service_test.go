7f7643ea51568f51fa5ef62249a3b853190b191a
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-02-09 17:50:20 +0700

    feat(injector-parameter): Add TestSimpleServiceSuccess, update TestSimpleService

--- test/simple_service_test.go
+++ test/simple_service_test.go
@@ -1,13 +1,19 @@
 package test
 
 import (
-	"fmt"
+	"github.com/stretchr/testify/assert"
 	"lopingbest/GolangRESTFullAPI/simple"
 	"testing"
 )
 
-func TestSimpleService(t *testing.T) {
-	simpleService, err := simple.InitializeService()
-	fmt.Println(err)
-	fmt.Println(simpleService)
+func TestSimpleServiceError(t *testing.T) {
+	simpleService, err := simple.InitializeService(true)
+	assert.NotNil(t, err)
+	assert.Nil(t, simpleService)
+}
+
+func TestSimpleServiceSuccess(t *testing.T) {
+	simpleservice, err := simple.InitializeService(false)
+	assert.Nil(t, err)
+	assert.NotNil(t, simpleservice)
 }
