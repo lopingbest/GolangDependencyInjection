4e283cfa32e492e0a442c3e1971bea1e88406337
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-02-16 19:04:27 +0700

    feat(Cleanup-Function): Add InitializedConnection

--- simple/injector.go
+++ simple/injector.go
@@ -85,3 +85,10 @@ func InitializedConfiguration() *Configuration {
 	)
 	return nil
 }
+
+func InitializedConnection(name string) (*Connection, func()) {
+	wire.Build(
+		NewConnection,
+		NewFile)
+	return nil, nil
+}
