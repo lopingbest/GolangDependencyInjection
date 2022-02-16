6eee72fd1ae3ef2a94d02442d242640da0ca263e
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-02-16 19:05:26 +0700

    feat(Cleanup-Function): Add file_test

--- test/file_test.go
+++ test/file_test.go
@@ -1,1 +1,14 @@
 package test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"lopingbest/GolangRESTFullAPI/simple"
+	"testing"
+)
+
+func TestConnection(t *testing.T) {
+	connection, cleanup := simple.InitializedConnection("Databases")
+	assert.NotNil(t, connection)
+
+	cleanup()
+}
