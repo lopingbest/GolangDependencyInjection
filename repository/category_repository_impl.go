9dafbbf27a94f6adef336f81d2f1a51dc5e6e2ea
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-02-20 11:37:05 +0700

    feat(Dependency-injection-Restfull): modified category_repository_impl

--- repository/category_repository_impl.go
+++ repository/category_repository_impl.go
@@ -8,26 +8,26 @@ import (
 	"lopingbest/GolangRESTFullAPI/model/domain"
 )
 
-type CategoryRespositoryImplementation struct {
+type CategoryRepositoryImplementation struct {
 }
 
-func NewCategoryRespositoryImplementation() CategoryRepository {
-	return &CategoryRespositoryImplementation{}
+func NewCategoryRepository() *CategoryRepositoryImplementation {
+	return &CategoryRepositoryImplementation{}
 }
 
-func (c *CategoryRespositoryImplementation) Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
+func (repository *CategoryRepositoryImplementation) Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
 	SQL := "insert into category(name) values (?)"
-	result, err := tx.ExecContext(ctx, SQL, category.Name) //category.name merupakan target
-	helper.PanicIfError(err)
-	id, err := result.LastInsertId() //mendapatkan id terakhir
+	result, err := tx.ExecContext(ctx, SQL, category.Name)
 	helper.PanicIfError(err)
 
-	//set category
+	id, err := result.LastInsertId()
+	helper.PanicIfError(err)
+
 	category.Id = int(id)
 	return category
 }
 
-func (c *CategoryRespositoryImplementation) Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
+func (repository *CategoryRepositoryImplementation) Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
 	SQL := "update category set name = ? where id = ?"
 	_, err := tx.ExecContext(ctx, SQL, category.Name, category.Id)
 	helper.PanicIfError(err)
@@ -35,14 +35,13 @@ func (c *CategoryRespositoryImplementation) Update(ctx context.Context, tx *sql.
 	return category
 }
 
-func (c *CategoryRespositoryImplementation) Delete(ctx context.Context, tx *sql.Tx, category domain.Category) {
+func (repository *CategoryRepositoryImplementation) Delete(ctx context.Context, tx *sql.Tx, category domain.Category) {
 	SQL := "delete from category where id = ?"
 	_, err := tx.ExecContext(ctx, SQL, category.Id)
 	helper.PanicIfError(err)
-
 }
 
-func (repository *CategoryRespositoryImplementation) FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error) {
+func (repository *CategoryRepositoryImplementation) FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error) {
 	SQL := "select id, name from category where id = ?"
 	rows, err := tx.QueryContext(ctx, SQL, categoryId)
 	helper.PanicIfError(err)
@@ -58,15 +57,13 @@ func (repository *CategoryRespositoryImplementation) FindById(ctx context.Contex
 	}
 }
 
-func (c *CategoryRespositoryImplementation) FindAll(ctx context.Context, tx *sql.Tx) []domain.Category {
+func (repository *CategoryRepositoryImplementation) FindAll(ctx context.Context, tx *sql.Tx) []domain.Category {
 	SQL := "select id, name from category"
 	rows, err := tx.QueryContext(ctx, SQL)
 	helper.PanicIfError(err)
 	defer rows.Close()
 
-	//data slice
 	var categories []domain.Category
-
 	for rows.Next() {
 		category := domain.Category{}
 		err := rows.Scan(&category.Id, &category.Name)
