fa1c61b78006aa440b2dcee5a2c9dcda7567f0b5
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-02-20 11:38:11 +0700

    feat(Dependency-injection-Restfull): Add injector

--- injector.go
+++ injector.go
@@ -1,1 +1,38 @@
+//go:build wireinject
+// +build wireinject
+
 package main
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/google/wire"
+	"github.com/julienschmidt/httprouter"
+	"lopingbest/GolangRESTFullAPI/app"
+	"lopingbest/GolangRESTFullAPI/controller"
+	"lopingbest/GolangRESTFullAPI/middleware"
+	"lopingbest/GolangRESTFullAPI/repository"
+	"lopingbest/GolangRESTFullAPI/service"
+	"net/http"
+)
+
+var categorySet = wire.NewSet(
+	repository.NewCategoryRepository,
+	wire.Bind(new(repository.CategoryRepository), new(*repository.CategoryRepositoryImplementation)),
+	service.NewCategoryService,
+	wire.Bind(new(service.CategoryService), new(*service.CategoryServiceImplementation)),
+	controller.NewCategoryController,
+	wire.Bind(new(controller.CategoryController), new(*controller.CategoryControllerImplementation)),
+)
+
+func InitializedServer() *http.Server {
+	wire.Build(
+		app.NewDB,
+		validator.New,
+		categorySet,
+		app.NewRouter,
+		wire.Bind(new(http.Handler), new(*httprouter.Router)),
+		middleware.NewAuthMiddleware,
+		NewServer,
+	)
+	return nil
+}
